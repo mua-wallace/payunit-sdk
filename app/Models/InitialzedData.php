9af696de6f14dfd8065d0ea13e90ca0768639295
mua-wallace <muawallizewi@gmail.com>
2022-03-21 15:22:42 +0100

    added  one-to-one relationship

--- app/Models/InitialzedData.php
+++ app/Models/InitialzedData.php
@@ -16,7 +16,18 @@ class InitialzedData extends Model
      */
     protected $table = 'initializedData';
     protected $fillable = ['t_url','t_id', 't_sum', 'transaction_id'];
+    public $appends = ['initialize'];
 
     public $timestamps = false;
+
+    public function initialize() {
+        return $this->hasOne(Initialze::class, 'transaction_id', 'transaction_id')->first();
+    }
+
+    public function getInitializeAttribute()
+    {
+        
+        return $this->initialize();
+    }
 
 }
