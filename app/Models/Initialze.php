3ea07c0ada74c37be9fad2c078ab874fc863f4b2
mua-wallace <muawallizewi@gmail.com>
2022-04-11 11:12:07 +0100

    revert changes

---
+++ app/Models/Initialze.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Initialze extends Model
+{
+    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'initializer';
+    protected $fillable = ['transaction_id','total_amount', 'currency', 'return_url', 'notify_url', 'name', 'description', 'purchaseRef' ];
+}
---
+++ database/migrations/2022_03_22_050244_create_mtnsucessdata_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMtnsucessdataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mtnsucessdata', function (Blueprint $table) {
+            $table->string('gateway');
+            $table->string('status');
+            $table->text('message');
+            $table->string('payment_ref');
+            $table->string('transaction_id')->primary();
+            $table->text('pay_token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mtnsucessdata');
+    }
+}
